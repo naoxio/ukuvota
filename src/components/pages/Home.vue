906f82635da2ce80b903bbe283f75217c40c7d17
wolfi <wolfgang@disroot.org>
2017-08-30 15:31:05 +0200

    add description field

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -6,20 +6,37 @@
     </center>
     <q-card>
       <q-card-main>
+        <q-alert
+          color="dark"
+          icon="toys"
+          appear
+          dismissible
+        >
+          optional fields are marked with an asterix (*)
+        </q-alert>
         <q-input v-model="text" float-label="Topic Question" />
-        <!-- Datetime Range -->
-
+        <q-input
+          v-model="area"
+          type="textarea"
+          float-label="* Description"
+          :max-height="50"
+          :min-rows="7"
+        />
+        <div style="text-align: right">
+          <q-btn icon="arrow forward">Next</q-btn>
+        </div>
       </q-card-main>
     </q-card>
   </main-layout>
 </template>
 <script>
 import MainLayout from '@/layouts/MainLayout'
-import { QBtn, QCard, QCardMain, QCardMedia, QCardSeperator, QCardTitle, QDatetimeRange, QField, QInput, QItem, QItemMain, QItemSide, QList, QModal, QModalLayout, QToolbar, QToolbarTitle, QTree } from 'quasar'
+import { QAlert, QBtn, QCard, QCardMain, QCardMedia, QCardSeperator, QCardTitle, QDatetimeRange, QField, QInput, QItem, QItemMain, QItemSide, QList, QModal, QModalLayout, QToolbar, QToolbarTitle, QTree } from 'quasar'
 
 export default {
   components: {
     MainLayout,
+    QAlert,
     QBtn,
     QCard,
     QCardMain,
