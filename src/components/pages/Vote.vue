a8fd6f5a5de3045a55b3a94c905f6af71071d870
wolfi <wolfi@disroot.org>
2017-09-08 20:55:11 +0200

    adjust emojis for score voting

--- src/components/pages/Vote.vue
+++ src/components/pages/Vote.vue
@@ -7,23 +7,23 @@
         <h5><q-field :label="topicQuestion"></q-field></h5>
         <q-field :label="topicDescription"></q-field>
         <div class="row justify-around">
-          <img src="statics/1f47f.svg" height="32px" />
-          <img src="statics/1f616.svg" height="32px" />
+          <img src="statics/1f621.svg" height="32px" />
+          <img src="statics/1f620.svg" height="32px" />
           <img src="statics/1f641.svg" height="32px" />
           <img src="statics/1f636.svg" height="32px" />
           <img src="statics/1f642.svg" height="32px" />
           <img src="statics/1f60a.svg" height="32px" />
-          <img src="statics/1f60d.svg" height="32px" />
+          <img src="statics/2764.svg" height="32px" />
         </div>
         </br>
         <div class="row justify-around">
           <q-radio v-model="radio1" val="-3" color="red"/>
-          <q-radio v-model="radio1" val="-2" color="secondary" style="margin-left: 10px" />
+          <q-radio v-model="radio1" val="-2" color="red" style="margin-left: 10px" />
           <q-radio v-model="radio1" val="-1" color="secondary" style="margin-left: 10px" />
           <q-radio v-model="radio1" val="0" color="secondary" style="margin-left: 10px" />
           <q-radio v-model="radio1" val="1" color="secondary" style="margin-left: 10px" />
-          <q-radio v-model="radio1" val="2" color="secondary" style="margin-left: 10px" />
-          <q-radio v-model="radio1" val="3" color="secondary" style="margin-left: 10px" />
+          <q-radio v-model="radio1" val="2" color="green" style="margin-left: 10px" />
+          <q-radio v-model="radio1" val="3" color="green" style="margin-left: 10px" />
         </div>
       </q-card-main>
     </q-card>
