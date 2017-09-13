ff25bbdd32fd6e182c66be884ec3dd823d0685b6
wolfi <wolfi@disroot.org>
2017-09-14 01:30:51 +0200

    made names into icons

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -30,15 +30,15 @@
         <center>
         <div class="row justify-center items-center">
           <div class="col-6">
-            <p class="title">Ukuvota</p>
-            <a href="https://douginamug.gitbooks.io/cooperative-decision-making-that-scales/content/" target="_blank">
-            reference</a><br>
-            <a href="https://gitlab.com/yunity/ukuvota/" target="_blank">source</a>
-          </div>
-          <div class="col-6">
             <a href="https://yunity.org" target="_blank">
               <img class="logo" src="statics/logo.png" width="128px" />
             </a>  
+          </div>
+          <div class="col-6">
+            <p class="title">Ukuvota</p>
+            <a href="https://douginamug.gitbooks.io/cooperative-decision-making-that-scales/content/" target="_blank">
+            <q-icon name="fa-book"></q-icon></a>
+            <a href="https://gitlab.com/yunity/ukuvota/" target="_blank"><q-icon name="fa-gitlab"></q-icon></a>
           </div>
         </div>
         <br></br>
