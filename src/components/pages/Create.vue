c68c36735c689d21fdcf77d946e94618a8c581d5
wolfi <wolfgang@disroot.org>
2017-09-18 02:51:18 +0200

    add negative score multipler dropdown in create vue

--- src/components/pages/Create.vue
+++ src/components/pages/Create.vue
@@ -1,5 +1,17 @@
 <template>
   <main-layout>
+    <q-modal
+      ref="weightingInfo"
+      :content-css="{padding: '50px', minWidth: '50vw'}"
+    >
+      <h4>Negative Score Multiplier</h4>
+      <p>
+        Score voting normally values scores as equal increments and chooses the option the highest total score. For example, in a situation where there are three ways to score ('Resist, accept, support') the score values would be -1, 0 and +1. However, on the basis that scoring represents resistance and support, using this equal weighting treats resistance as 'negative support' and selects the option where R < A < S.
+<br>
+The simplest way to select the option where R << A < S is to weight resistance scores more heavily than support scores. Using a weighting factor of 3 would value non-resistance twice as favorably as support. It might be tempting to increase thie weighting factor to something in an attempt to avoid resistance however do so has consequences. If a weighting factor approaches the point where resistance from one individual outweighs all potential support in the group, the voting system degenerates such that only resistance scores carry decision-making value. This potentially denies a supermajority of individuals from pursuing their interests and forces them to vote dishonestly (only in terms of resistance), become more exclusive/exclusionary or leave. So the weighting factor must be greater than 1 and significantly less than the number of individuals. In smaller groups the weighting factor is likely to be higher and in larger groups it is likely to be lower.
+      </p>
+      <q-btn color="primary" @click="$refs.weightingInfo.close()">Close</q-btn>
+    </q-modal>
     <q-card style="max-width: 700px; text-align: left;">
       <q-card-main>
         <q-field 
@@ -10,6 +22,16 @@
             :error="topicMissing"
             />
         </q-field>
+        <div class="row">
+          <q-select class="col-11" v-model="negativeScoreWeight"
+            float-label="Negative Score Multiplier"
+            :options="negativeMultipliers"
+          />
+          <div class="col-1 info">
+            <q-icon @click="$refs.weightingInfo.open()" color="primary" name="fa-info" 
+            ></q-icon>
+          </div>
+        </div>
         <p class="caption row justify-between">
           Proposal Collection Time
           <q-chip>
@@ -52,7 +74,7 @@
 </template>
 <script>
 import MainLayout from '@/layouts/MainLayout'
-import { date, uid, QAlert, QBtn, QCard, QCardMain, QCardMedia, QCardTitle, QChip, QField, QInput, QInlineDatetime, QItem, QItemMain, QItemSide, QList, QSelect, QSlider } from 'quasar'
+import { date, uid, QAlert, QBtn, QCard, QCardMain, QCardMedia, QCardTitle, QChip, QField, QIcon, QInput, QInlineDatetime, QItem, QItemMain, QItemSide, QList, QModal, QSelect, QSlider } from 'quasar'
 import { setTopic } from '@/data'
 import { buildOutput } from '@/timer'
 
@@ -69,12 +91,14 @@ export default {
     QCardTitle,
     QChip,
     QField,
+    QIcon,
     QInlineDatetime,
     QInput,
     QItem,
     QItemMain,
     QItemSide,
     QList,
+    QModal,
     QSelect,
     QSlider
   },
@@ -109,6 +133,7 @@ export default {
             'Change Nothing': 'keep things the way they are',
             'Repeat Process': 'look for other options and repeat the process'
           },
+          'negativeScoreWeight': this.negativeScoreWeight,
           'emojis': {
             'Change Nothing': 0,
             'Repeat Process': 0
@@ -135,11 +160,51 @@ export default {
       proposalDays: 2,
       votingMinutes: 1,
       votingHours: 0,
-      votingDays: 1
+      votingDays: 1,
+      negativeScoreWeight: 3,
+      negativeMultipliers: [
+        {
+          label: 'x1',
+          value: 1
+        },
+        {
+          label: 'x2',
+          value: 2
+        },
+        {
+          label: 'x3',
+          value: 3
+        },
+        {
+          label: 'x4',
+          value: 4
+        },
+        {
+          label: 'x5',
+          value: 5
+        },
+        {
+          label: 'x6',
+          value: 6
+        },
+        {
+          label: 'x7',
+          value: 7
+        },
+        {
+          label: 'x8',
+          value: 8
+        }
+      ]
     }
   }
 }
 </script>
 <style lang="stylus">
-
+.info {
+  margin auto
+  cursor pointer
+  text-align center
+  font-size 14px
+}
 </style>
