ebb3159a6ec65de037771e080548036fecff4c41
wolfi <wolfi@disroot.org>
2017-09-23 03:53:03 +0200

    random changes that are all pushed together -nick

--- server/index.js
+++ server/index.js
@@ -2,17 +2,22 @@ const PouchDB = require('pouchdb')
 const express = require('express')
 const mkdirp = require('mkdirp')
 const app = express()
+const morgan = require('morgan')
 const { join } = require('path')
 
 const BASE_DIR = join(__dirname, '..')
 const DB_DIR = process.argv[2] || join(BASE_DIR, 'mydb/')
+const PORT = process.argv[3] || process.env.PORT || 3000
+
+app.use(morgan('combined'))
+app.use(express.static(join(BASE_DIR, 'dist')))
 
 const start = () => {
-  app.use(express.static(join(BASE_DIR, 'dist')))
-
   app.use('/db', require('express-pouchdb')(PouchDB.defaults({ prefix: DB_DIR })))
 
-  app.listen(3000)
+  app.listen(PORT, () => {
+    console.log('Listening on port:', PORT)
+  })
 }
 
 mkdirp(DB_DIR, err => {
