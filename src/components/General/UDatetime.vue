5276e194b3afcf21a8c68cb89d2162d168ca572e
wolfi <wolfi@disroot.org>
2017-10-29 11:54:28 +0100

    remove selectors

--- src/components/General/UDatetime.vue
+++ src/components/General/UDatetime.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <q-select :float-label="$t('Date.Day')" v-model="selectedDay" :options="days"/>
-    <q-select :float-label="$t('Date.Month')" v-model="selectedMonth" :options="months"/>
     <q-input :stack-label="label" v-model="date" :value="date">
       <q-icon name="today">
         <q-popover ref="popover">
@@ -30,13 +28,7 @@
       value (val) { this.$store.dispatch(this.updFnc, val) }
     },
     computed: {
-      date () { return this.$store.getters[this.getFormatted] },
-      months () {
-        let months = []
-      },
-      days () {
-
-      }
+      date () { return this.$store.getters[this.getFormatted] }
     },
     components: {
       QBtn,
@@ -52,21 +44,7 @@
         updFnc: 'update' + this.store,
         getFormatted: 'get' + this.store + 'Formatted',
         getFnc: 'get' + this.store,
-        selectedMonth: 'Nov',
-        months: [
-          { label: this.$t('Month.Jan'), value: 1 },
-          { label: this.$t('Month.Feb'), value: 2 },
-          { label: this.$t('Month.Mar'), value: 3 },
-          { label: this.$t('Month.Apr'), value: 4 },
-          { label: this.$t('Month.May'), value: 5 },
-          { label: this.$t('Month.Jun'), value: 6 },
-          { label: this.$t('Month.Jul'), value: 7 },
-          { label: this.$t('Month.Aug'), value: 8 },
-          { label: this.$t('Month.Sep'), value: 9 },
-          { label: this.$t('Month.Oct'), value: 10 },
-          { label: this.$t('Month.Nov'), value: 11 },
-          { label: this.$t('Month.Dec'), value: 12 }
-        ]
+        selectedMonth: 11
       }
     }
   }
