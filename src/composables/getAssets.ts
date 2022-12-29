2a16c1bd20f4a19c04d89c15e956488d5f8f79bb
waotzi <waotzi@proton.me>
2022-12-29 10:29:39 +0700

    add legacy redirect

--- src/composables/getAssets.ts
+++ src/composables/getAssets.ts
@@ -5,7 +5,6 @@ interface Icon {
 export function getAssets(globs: any[]) {
     const assets: Icon[] = []
     globs.forEach(glob => {
-        console.log(glob.default.split('/').pop())
         assets.push({
             name: glob.default.split('/').pop().split('.').shift(),
             src: glob.default
