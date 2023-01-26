03c61e2433dbdf8953920efa2ade61180af77ed2
waotzi <waotzi@proton.me>
2023-01-26 17:12:14 +0800

    new countdown and time cards wip

--- src/composables/quickTestData.ts
+++ src/composables/quickTestData.ts
@@ -58,8 +58,8 @@ voterNames.forEach((voterName: string, i: number) => {
 
 export function quickTest() {
     return {
-        proposalEnd: +new Date() + 100000,
-        votingEnd: +new Date() + 200000,
+        proposalDates: [+new Date() + 100000, +new Date() + 20000000],
+        votingDates: [+new Date() + 30000000, +new Date() + 50000000],
         weighting: '3',
         title: 'What pizza toppings should we order for our office party?',
         description: 'We are planning an office party and would like to order pizza for everyone. We are trying to decide on the toppings and would like some ideas. Please suggest your favorite toppings and any unique combinations you think would be delicious!',
