3b66a6aab456a0ea72a77f91eebc2ebe3a4c0bfb
waotzi <waotzi@proton.me>
2023-02-27 15:00:00 +0700

    add astro compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import vue from "@astrojs/vue";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [astroI18next(), tailwind(), vue({ appEntrypoint: '/src/pages/_app' })],
+  integrations: [astroI18next(), tailwind(), vue({
+    appEntrypoint: '/src/pages/_app'
+  }), compress()],
   adapter: node({
     mode: "standalone"
   })
