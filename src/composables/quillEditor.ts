6e98ff3f754a98d2ea9dec787e94bce4c499880a
waotzi <waotzi@proton.me>
2023-03-27 00:43:23 +0700

    make more readable

---
+++ src/composables/quillEditor.ts
@@ -0,0 +1,24 @@
+import Quill from 'quill'
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image'],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+];
+const options = {
+    debug: 'info',
+    modules: {
+      toolbar: toolbarOptions,
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow',
+};
+
+export function createQuill(element: string) {
+    return new Quill(element, options);
+}
+
