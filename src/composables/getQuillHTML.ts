08c2a4f0fcab92a6497e17506ad75fa227875573
waotzi <waotzi@proton.me>
2023-04-10 03:03:04 +0700

    fix description field

--- src/composables/getQuillHTML.ts
+++ src/composables/getQuillHTML.ts
@@ -1,9 +1,16 @@
 import { Delta } from 'quill';
 import createQuill from './createQuill';
 import updateQuill from './updateQuill';
+import DOMPurify from 'dompurify';
 
 export default function getQuillHTML(content: string | Delta): string {
   let quill = createQuill('#temp');
   quill = updateQuill(quill, content);
-  return document.querySelector('#temp').querySelector('.ql-editor').innerHTML;
+  const contentHTML = document.querySelector('#temp .ql-editor').innerHTML;
+  const decodedHTML = decodeURIComponent(contentHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+  const sanitizedHTML = DOMPurify.sanitize(decodedHTML);
+  const fragment = document.createRange().createContextualFragment(sanitizedHTML);
+  const tempElement = document.createElement('div');
+  tempElement.appendChild(fragment);
+  return tempElement.innerHTML;
 }
