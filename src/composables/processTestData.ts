16a601155f26133415134b68829ad316a8c13abd
waotzi <waotzi@proton.me>
2023-04-09 17:03:18 +0700

    fix results page

--- src/composables/processTestData.ts
+++ src/composables/processTestData.ts
@@ -14,18 +14,18 @@ const proposals: IProposal[] = [
     },
     {
         id: 'a4750500-16b3-a8d4-d38b-3808bb4b29566f0a',
-        title: 'BBQ chicken with red onions',
-        description: 'This proposal suggests a BBQ chicken pizza with red onions as the toppings.'
+        title: '',
+        description: {"ops":[{"insert":""}]}
     },
     {
         id: 'f6fe4552-5677-ab2c-0c92-b4811e8bf22e7b21',
-        title: 'Signature pizza',
-        description: 'This proposal suggests ordering a pizza with a variety of toppings, such as pepperoni, mushroom, sausage, bell peppers, and olives.'
+        title: '',
+        description: {"ops":[]}
     },
     {
         id: '2e1b5019-32eb-25ca-2c88-7240fd91612742ae',
-        title: 'Build your own',
-        description: 'This proposal suggests allowing each person to choose their own toppings for their individual pizza.'
+        title: '',
+        description: ''
     }
 ]
 
@@ -59,7 +59,7 @@ voterNames.forEach((voterName: string, i: number) => {
 export default function processTest() {
     return {
         proposalDates: [+new Date(), +new Date()],
-        votingDates: [+new Date() + 200000, +new Date() + 4000000],
+        votingDates: [+new Date(), +new Date()],
         weighting: '3',
         title: 'What pizza toppings should we order for our office party?',
         description: { ops: [ {"insert": "We are planning an office party and would like to order pizza for everyone. We are trying to decide on the toppings and would like some ideas. Please suggest your favorite toppings and any unique combinations you think would be delicious!"}]},
