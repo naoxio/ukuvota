e0f5dc39f6dfe1ffba1125fa57eeff5d17edae59
waotzi <waotzi@proton.me>
2023-04-12 15:16:52 +0700

    update to use pouchdb

--- server/db.mjs
+++ server/db.mjs
@@ -1,30 +1,17 @@
-import { Level } from 'level'
+import PouchDB from 'pouchdb'
 
-const db = new Level('db', { repair: true });
+const db = new PouchDB('db');
 
-try {
-  await db.open();
-  console.log('Database opened successfully.');
-} catch (error) {
-  console.error('Failed to open database:', error);
-  if (error.code === 'LEVEL_LOCKED') {
-    console.log('Another process or instance has opened the database.');
-  } else if (error.code === 'LEVEL_IO_ERROR') {
-    console.error('Database error:', error);
-    console.log('Please check if the database file exists and has the correct permissions.');
+
+// Open the database
+(async () => {
+  try {
+    await db.info(); // Using info() to check if the database is accessible
+    console.log('Database opened successfully.');
+  } catch (error) {
+    console.error('Failed to open database:', error);
+    process.exit(1);
   }
-  process.exit(1);
-}
-
-// Compact the database to reduce disk usage
-db.on('ready', () => {
-  db.compactRange([], (err) => {
-    if (err) {
-      console.error('Failed to compact database:', err);
-    } else {
-      console.log('Database compacted successfully.');
-    }
-  });
-});
+})();
 
 export default db;
