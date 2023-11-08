eac6ee537c7ebed06036b1403bb8b3440fb5e173
Martin Schiller <waotzi@proton.me>
2023-11-08 10:28:19 +0700

    add dashboard floating button

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,5 +1,20 @@
 import { atom } from 'nanostores';
 
-export const theme = atom('dark');
-export const firstVisit = atom(true);
-export const processes = atom([]);
+const theme = atom('dark');
+const firstVisit = atom(true);
+const processes = atom<string[]>([]);
+
+const addProcess = (uuid: string) => {
+  const currentProcesses = processes.get();
+  if (!currentProcesses.includes(uuid)) {
+    processes.set([...currentProcesses, uuid]);
+  }
+};
+
+const addProcesses = (uuids: string[]) => {
+  const currentProcesses = processes.get();
+  const newProcesses = uuids.filter(uuid => !currentProcesses.includes(uuid));
+  processes.set([...currentProcesses, ...newProcesses]);
+};
+
+export { addProcess, addProcesses, processes, firstVisit, theme };
