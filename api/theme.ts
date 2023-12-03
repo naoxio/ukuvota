def91b7cedc6867491dce26cc240de926add7229
Martin Schiller <waotzi@proton.me>
2023-12-03 11:29:43 +0700

    fix theme

--- api/theme.ts
+++ api/theme.ts
@@ -1,5 +1,10 @@
 // /api/theme.js
-export default function(req, res) {
+export default async (req: any, res: any) => {
+  if (req.method !== 'POST') {
+    res.status(405).end(); // Method Not Allowed
+    return;
+  }
+
   const newTheme = req.body.theme;
 
   res.setHeader('Set-Cookie', `theme=${newTheme}; Path=/; HttpOnly; SameSite=Strict`);
