2666fbeb685a416f5202e4caefd27a39233f7a3a
Martin Schiller <waotzi@proton.me>
2023-12-07 17:43:36 +0700

    fix theme switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,19 @@
 import astroI18next from "astro-i18next";
 import { defineConfig } from 'astro/config';
-import Icons from 'unplugin-icons/vite'
+import Icons from 'unplugin-icons/vite';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [astroI18next(), tailwind()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   vite: {
-    plugins: [
-      Icons({
-        compiler: 'astro',
-      }),
-    ],
+    plugins: [Icons({
+      compiler: 'astro'
+    })]
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
---
+++ src/pages/api/theme.ts
@@ -0,0 +1,20 @@
+// /api/theme.js
+import type { APIRoute } from "astro"
+
+export const POST: APIRoute = async({ request, redirect }) => {
+  const formData = await request.formData();
+  const theme = formData.get('theme');
+  const headers = new Headers({
+    'Set-Cookie': `theme=${theme}; Path=/; HttpOnly; SameSite=Strict`,
+    'Content-Type': 'application/json'
+  });
+
+  const referrer = request.headers.get('referer') || '/';
+  headers.append('Location', referrer);
+
+  return new Response(null, {
+    status: 303, // 303 See Other
+    headers: headers,
+  });
+};
+
