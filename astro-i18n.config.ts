3d8c221e77a609d75aa3ad7aa80416c23e68dcdb
Martin Schiller <waotzi@proton.me>
2023-12-07 22:09:55 +0700

    fix lang switcher

--- astro-i18n.config.ts
+++ astro-i18n.config.ts
@@ -2,7 +2,7 @@ import { defineAstroI18nConfig } from "astro-i18n"
 
 export default defineAstroI18nConfig({
 	primaryLocale: "en", // default app locale
-	secondaryLocales: [], // other supported locales
+	secondaryLocales: ["de"], // other supported locales
 	fallbackLocale: "en", // fallback locale (on missing translation)
 	trailingSlash: "never", // "never" or "always"
 	run: "client+server", // "client+server" or "server"
--- astro-i18next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroI18nextConfig } from "astro-i18n";
-
-const config: AstroI18nextConfig = {
-  defaultLocale: "en",
-  locales: ["en", "de"]
-};
-
-export default config;
