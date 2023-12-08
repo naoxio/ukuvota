c51d95829a03a15d8d399c634d69073060e40dcc
Martin Schiller <waotzi@proton.me>
2023-12-09 00:33:23 +0700

    update switch stepper

---
+++ src/pages/api/update-step.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  const formData = await request.formData();
+  const step = formData.get('step');
+
+  let processCookie = request.headers.get('cookie');
+  let processCookieObject = processCookie ? JSON.parse(decodeURIComponent(processCookie.split('; ').find(row => row.startsWith('process='))?.split('=')[1] || '{}')) : {};
+
+  processCookieObject.step = step;
+
+  const headers = new Headers({
+    'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
+    'Content-Type': 'application/json',
+    'Location': '/create'
+  });
+  return new Response(null, { status: 303, headers: headers });
+};
