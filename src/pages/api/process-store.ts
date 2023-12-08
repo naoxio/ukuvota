2bb162c1a79cbcbb12147a8a196cd1fcc8596ad2
Martin Schiller <waotzi@proton.me>
2023-12-08 22:09:25 +0700

    unify process setup

--- src/pages/api/process-store.ts
+++ src/pages/api/process-store.ts
@@ -2,14 +2,15 @@ import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
   const formData = await request.formData();
-  const step = formData.get('step');
-
+  const step = Number(formData.get('step') || 1);
+  const nextStep = step + 1;
   const processCookieRaw = request.headers.get('cookie');
   let processCookieObject = processCookieRaw ? JSON.parse(decodeURIComponent(processCookieRaw.split('; ').find(row => row.startsWith('process='))?.split('=')[1] || '{}')) : {};
 
-  if (step === "1") {
+  if (step === 1) {
     try {
       Object.assign(processCookieObject, {
+        step: nextStep,
         weighting: formData.get('weighting'),
         title: formData.get('topicQuestion'),
         nojsdescription: formData.get('nojsdescription'),
@@ -21,25 +22,59 @@ export const POST: APIRoute = async ({ request }) => {
         'Content-Type': 'application/json'
       });
 
-      headers.append('Location', '/create/phases');
+      headers.append('Location', '/create');
       return new Response(null, { status: 303, headers: headers });
 
     } catch (error) {
       console.error("Error:", error);
       return new Response(null, { status: 303, headers: { 'Location': '/create' } });
     }
-  } else if (step === "2") {
+  } else if (step === 2) {
     try {
       // Update only the necessary field for step 2
       processCookieObject.phase = formData.get('phase');
+      processCookieObject.step = nextStep;
 
       const headers = new Headers({
         'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
         'Content-Type': 'application/json'
       });
 
-      let redirectPath = processCookieObject.phase === 'full' ? '/create/full-process' : '/create/voting-only';
-      headers.append('Location', redirectPath);
+      headers.append('Location', '/create');
+      return new Response(null, { status: 303, headers: headers });
+
+    } catch (error) {
+      console.error("Error:", error);
+      return new Response(null, { status: 303, headers: { 'Location': '/create' } });
+    }
+  } else if (step === 3) {
+    try {
+      const now = new Date();
+
+        let startDate = new Date(formData.get('startDate') as string);
+        let endDate = new Date(formData.get('endDate') as string);
+
+        if (isNaN(startDate.getTime()) || startDate < now) {
+          startDate = new Date(now);
+        }
+
+        if (isNaN(endDate.getTime()) || endDate <= startDate) {
+          endDate = new Date(startDate);
+          endDate.setSeconds(endDate.getSeconds() + 1);
+        }
+
+      Object.assign(processCookieObject, {
+        step: nextStep,
+        startDate: startDate.getTime(),
+        endDate: endDate.getTime()
+      });
+
+      const headers = new Headers({
+        'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
+        'Content-Type': 'application/json'
+      });
+
+      headers.append('Location', '/create');
       return new Response(null, { status: 303, headers: headers });
 
     } catch (error) {
@@ -47,7 +82,5 @@ export const POST: APIRoute = async ({ request }) => {
       return new Response(null, { status: 303, headers: { 'Location': '/create' } });
     }
   }
-
-  // If the step is neither "1" nor "2"
   return new Response(JSON.stringify({ message: "Invalid step" }), { status: 400 });
 };
