6dec5db5ad3a08bf2cff93d5e3054b4568e63a46
Martin Schiller <waotzi@proton.me>
2023-12-09 19:04:38 +0700

    clean theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,42 +1,42 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-			extend: {},
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-			styled: true,
-			themes: [
-					{
-							light: {
-									...require("daisyui/src/theming/themes")["[data-theme=business]"],
-									"primary": "#2a5d9a",
-									"secondary": "#3a7ab6",
-									"accent": "#feefc3",
-									"neutral": "#111",
-									"base-100": "#ffffff",
-									"base-200": "#f7f7f7",
-									"info": "#0072F5",
-									"success": "#439243",
-									"warning": "#e79230",
-									"error": "#E23636",
-							},
-							dark: {
-									...require("daisyui/src/theming/themes")["[data-theme=business]"],
-									"primary": "#2a5d9a",
-									"secondary": "#3a7ab6",
-									"accent": "#41331c",
-									"neutral": "#eee",
-									"base-100": "#323639",
-									"base-200": "#40474d",
-									"info": "#0072F5",
-									"success": "#61D161",
-									"warning": "#EDC25E",
-									"error": "#E23636",
-							},
-					}
-			],
-			prefix: "",
-			darkTheme: "dark",
-	},
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('daisyui')],
+    daisyui: {
+        styled: true,
+        themes: [
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["[data-theme=business]"],
+                    "primary": "#1e4a72",
+                    "secondary": "#295d85",
+                    "accent": "#feefc3",
+                    "neutral": "#2a2e33",
+                    "base-100": "#ffffff",
+                    "base-200": "#f4f5f7",
+                    "info": "#005ecb",
+                    "success": "#4a8b4a",
+                    "warning": "#c7a046",
+                    "error": "#c43232",
+                },
+                dark: {
+                    ...require("daisyui/src/theming/themes")["[data-theme=business]"],
+                    "primary": "#1e4a72",
+                    "secondary": "#295d85",
+                    "accent": "#4a3424",
+                    "neutral": "#e0e0e0",
+                    "base-100": "#1c2025",
+                    "base-200": "#2a2e33",
+                    "info": "#005ecb",
+                    "success": "#4a8b4a",
+                    "warning": "#c7a046",
+                    "error": "#c43232",
+                },
+            }
+        ],
+        prefix: "",
+        darkTheme: "dark",
+    },
 }
