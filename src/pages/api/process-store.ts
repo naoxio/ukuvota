2b98edb374a710ee4d81e8976530dfc8c917ae28
Waozi <waotzi@proton.me>
2024-01-09 18:18:54 +0700

    update time for voting phase

--- src/pages/api/process-store.ts
+++ src/pages/api/process-store.ts
@@ -36,50 +36,69 @@ export const POST: APIRoute = async ({ request }) => {
     }
   } else if (step === 2) {
     const phase = processCookieObject.phase;
+
+    const currentDate = new Date().getTime();
+    let startProposalDate = formData.get('start-date-picker-proposal') ? new Date(formData.get('start-date-picker-proposal') as string).getTime() : processCookieObject.startProposalDate || currentDate;
+    let endProposalDate = formData.get('end-date-picker-proposal') ? new Date(formData.get('end-date-picker-proposal') as string).getTime() : processCookieObject.endProposalDate || (startProposalDate + 3600000); // +1 hour
+    let startVotingDate = formData.get('start-date-picker-voting') ? new Date(formData.get('start-date-picker-voting') as string).getTime() : processCookieObject.startVotingDate || endProposalDate;
+    let endVotingDate = formData.get('end-date-picker-voting') ? new Date(formData.get('end-date-picker-voting') as string).getTime() : processCookieObject.endVotingDate || (startVotingDate + 3600000); // +1 hour
+    
+    console.log(startVotingDate)
+    console.log( new Date(formData.get('start-date-picker-voting') as string).getTime())
+    if (endProposalDate <= startProposalDate) {
+      endProposalDate = startProposalDate + 60000;
+    }
+
+    if (startVotingDate < endProposalDate) {
+      startVotingDate = endProposalDate;
+    }
+
+    if (endVotingDate <= startVotingDate) {
+        endVotingDate = startVotingDate + 60000;
+    }
+
     if (phase === 'full') {
-        try {
-          const currentDate = new Date().getTime();
-          let startProposalDate = formData.get('start-date-picker-proposal') ? new Date(formData.get('start-date-picker-proposal') as string).getTime() : processCookieObject.startProposalDate || currentDate;
-          let endProposalDate = formData.get('end-date-picker-proposal') ? new Date(formData.get('end-date-picker-proposal') as string).getTime() : processCookieObject.endProposalDate || (startProposalDate + 3600000); // +1 hour
-          let startVotingDate = formData.get('start-date-picker-voting') ? new Date(formData.get('start-date-picker-voting') as string).getTime() : processCookieObject.startVotingDate || endProposalDate;
-          let endVotingDate = formData.get('end-date-picker-voting') ? new Date(formData.get('end-date-picker-voting') as string).getTime() : processCookieObject.endVotingDate || (startVotingDate + 3600000); // +1 hour
+      try {
+        processCookieObject.startProposalDate = startProposalDate;
+        processCookieObject.endProposalDate = endProposalDate;
+        processCookieObject.startVotingDate = startVotingDate;
+        processCookieObject.endVotingDate = endVotingDate;
 
-          if (endProposalDate <= startProposalDate) {
-            endProposalDate = startProposalDate + 60000;
-          }
-
-          if (startVotingDate < endProposalDate) {
-            startVotingDate = endProposalDate;
-          }
-
-          if (endVotingDate <= startVotingDate) {
-              endVotingDate = startVotingDate + 60000;
-          }
-
-          processCookieObject.startProposalDate = startProposalDate;
-          processCookieObject.endProposalDate = endProposalDate;
-          processCookieObject.startVotingDate = startVotingDate;
-          processCookieObject.endVotingDate = endVotingDate;
-
-          if (!formData.get('nojsSubmission')) {
-            processCookieObject.step = nextStep.toString();
-          }
-
-          
-          const headers = new Headers({
-            'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
-            'Content-Type': 'application/json',
-            'Location': referer
-          });
-
-          return new Response(null, { status: 303, headers: headers });
-        } catch (error) {
-          console.error("Error:", error);
-          return new Response(null, { status: 303, headers: { 'Location': referer} });
+        if (!formData.get('nojsSubmission')) {
+          processCookieObject.step = nextStep.toString();
         }
         
-    } else if (phase === 'voting') {
-      return new Response(null, { status: 303, headers: { 'Location': referer } });
+        const headers = new Headers({
+          'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
+          'Content-Type': 'application/json',
+          'Location': referer
+        });
+
+        return new Response(null, { status: 303, headers: headers });
+      } catch (error) {
+        console.error("Error:", error);
+        return new Response(null, { status: 303, headers: { 'Location': referer} });
+      }
+    } else if (phase === 'voting') { 
+      try {
+        processCookieObject.startVotingDate = startVotingDate;
+        processCookieObject.endVotingDate = endVotingDate;
+
+        if (!formData.get('nojsSubmission')) {
+          processCookieObject.step = nextStep.toString();
+        }
+        
+        const headers = new Headers({
+          'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
+          'Content-Type': 'application/json',
+          'Location': referer
+        });
+
+        return new Response(null, { status: 303, headers: headers });
+      } catch (error) {
+        console.error("Error:", error);
+        return new Response(null, { status: 303, headers: { 'Location': referer} });
+      }
     } else {
       return new Response(null, { status: 303, headers: { 'Location': referer } });
     }
