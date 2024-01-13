340d494b313d7c3c65e21ae3ee584c0491b2f047
Waozi <waotzi@proton.me>
2024-01-13 17:31:48 +0700

    update translations

--- src/pages/api/create-process.ts
+++ src/pages/api/create-process.ts
@@ -4,11 +4,12 @@ import { parseProcessRawCookie } from '@utils/parseProcessCookie';
 export const POST: APIRoute = async ({ request }) => {
   let processCookieObject = parseProcessRawCookie(request.headers.get('cookie'));
   processCookieObject.create = 'true';
+  const referer = request.headers.get('referer') as string;
 
   const headers = new Headers({
     'Set-Cookie': `process=${encodeURIComponent(JSON.stringify(processCookieObject))}; Path=/; HttpOnly; SameSite=Strict`,
     'Content-Type': 'application/json',
-    'Location': request.headers.get('origin') as string + '/create'
+    'Location': referer + 'create'
   });
 
   return new Response(null, { status: 303, headers: headers });
