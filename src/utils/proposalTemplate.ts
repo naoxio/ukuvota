22c3c14368a2807764d7ad386d29bf9b93b51e7d
waozi <waotzi@proton.me>
2024-04-10 01:48:25 +0700

    add toggle mode edit view mode proposals

--- src/utils/proposalTemplate.ts
+++ src/utils/proposalTemplate.ts
@@ -18,17 +18,22 @@ const createProposalElement = (
 
   proposalElement.innerHTML = `
     <div class="flex items-center flex-col" id="${uniqueId}">
-      <div class="flex flex-col w-full">
+      <div class="flex flex-col w-full edit-mode">
         <b>Title</b>
         <input id="title-${uniqueId}" type="text" class="input input-bordered input-sm my-2 w-full" value="${title}" />
         <label>Description</label>
         <div id="description-${uniqueId}">${descriptionContent}</div>
         <input type="hidden" id="quillops-${uniqueId}" />
       </div>
+      <div class="flex flex-col w-full view-mode" style="display:none">
+        <h1>${title}</h1>
+        <p>${descriptionContent}</p>
+      </div>
     </div>
     <div class="flex justify-center w-full pt-2">
-      <button class="edit-button btn btn-primary btn-sm">${isSetup ? 'Edit' : 'Save'}</button>
-      <button class="delete btn btn-ghost text-error btn-xs" style="${isSetup ? 'display:none;' : 'display:block;'}">Delete</button>
+      <button class="edit-button btn btn-primary btn-sm" style="${isSetup ? 'display:block;' : 'display:none;'}">Edit</button>
+      <button class="save-button btn btn-primary btn-sm" style="${isSetup ? 'display:none;' : 'display:block;'}">Save</button>
+      <button class="delete btn btn-ghost text-error btn-xs" style="display:block;">Delete</button>
     </div>
   `;
   return proposalElement;
