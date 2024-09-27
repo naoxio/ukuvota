65c06ed5344c264d1efd9f7fe39bf4fbe5296aad
WaoziXyz <waotzi@proton.me>
2024-09-27 12:09:14 +0700

    fix astro version

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyBijzjkWVepv9FxY3Rc1xE7NdgjWQc0PwM",
+  authDomain: "impactful-yeti-387506.firebaseapp.com",
+  databaseURL: "https://impactful-yeti-387506-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "impactful-yeti-387506",
+  storageBucket: "impactful-yeti-387506.appspot.com",
+  messagingSenderId: "259081600268",
+  appId: "1:259081600268:web:cd2e0d4d312f20c7205d29"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const firebaseDB = getDatabase(app);
